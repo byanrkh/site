d45144eb3d66f12289b42ca1cf70f102ea75fb13
abydityadev <abyan.raditya@icloud.com>
2025-01-19 19:28:56 +0700

    feat: add hero section to the homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Grotesk } from "@/libs/font";
+import { Grotesk, Mono } from "@/libs/font";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
--- libs/font.ts
+++ libs/font.ts
@@ -1,5 +1,10 @@
-import { Space_Grotesk } from 'next/font/google'
+import { IBM_Plex_Mono, Space_Grotesk } from 'next/font/google'
 
 export const Grotesk = Space_Grotesk({
     subsets: ['latin']
-})
+});
+
+export const Mono = IBM_Plex_Mono({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700']
+});
