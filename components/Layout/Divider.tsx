2d4e9330e876e61ae2cc588bbc730e4ca1a85473
abydityadev <abyan.raditya@icloud.com>
2025-01-19 21:20:56 +0700

    style: thicken divider for better visibility

--- components/Layout/Divider.tsx
+++ components/Layout/Divider.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Divider() {
   return (
-    <div className="h-[0.5] my-20 bg-gradient-to-r from-background via-[#39393f] to-background"></div>
+    <div className="h-[1px] my-20 bg-gradient-to-r from-10% to-90% from-background via-zinc-600 to-background"></div>
   );
 }
