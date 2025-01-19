cd0afd135f48cd30c9523f128f2ad4643c48aba7
abydityadev <abyan.raditya@icloud.com>
2025-01-19 21:37:33 +0700

    feat: add new about page

--- components/UI/Navbar.tsx
+++ components/UI/Navbar.tsx
@@ -15,6 +15,10 @@ export default function Navbar() {
       path: "/",
     },
     {
+      label: "About",
+      path: "/about",
+    },
+    {
       label: "blog",
       path: "/blog",
     },
