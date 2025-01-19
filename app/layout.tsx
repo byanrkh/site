2abccbe84f8de699c89bc8f7e9a2869e5735030e
abydityadev <abyan.raditya@icloud.com>
2025-01-19 10:09:37 +0700

    feat: add fonts and integrate analytics & performance insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Grotesk } from "@/libs/font";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${Grotesk.className} antialiased`}>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
---
+++ libs/font.ts
@@ -0,0 +1,5 @@
+import { Space_Grotesk } from 'next/font/google'
+
+export const Grotesk = Space_Grotesk({
+    subsets: ['latin']
+})
