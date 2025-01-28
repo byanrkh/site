7926909c46a50592e7800920052179898eb47c54
abydityadev <abyan.raditya@icloud.com>
2025-01-28 16:53:55 +0700

    Feat: Project is now accurate

--- app/(root)/(home)/content/Projects.tsx
+++ app/(root)/(home)/content/Projects.tsx
@@ -3,36 +3,51 @@ import Link from "next/link";
 import React from "react";
 
 export default function Projects() {
+  const project = [
+    {
+      label: "Personal Website",
+      summary:
+        "a digital space that showcases a person&#39;s best work, blog, backgrounds, etc.",
+      tags: ["NextJS"],
+      link: "https://github.com/byanrkh/site",
+    },
+    {
+      label: "MDX Blog",
+      summary: "Minimal looking simple mdx Blog system using NextJS!",
+      tags: ["NextJS", "MDX"],
+      link: "https://github.com/byanrkh/mdx-blog",
+    },
+  ];
+
   return (
     <section>
       <Title>Projects</Title>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
-        <Link
-          href={"https://byanrkh.my.id"}
-          className="border border-[#202024] p-4 rounded group hover:scale-[1.02] duration-75"
-        >
-          <h1 className="group-hover:underline">Portfolio</h1>
-          <p className="mt-2 mb-4 text-sm text-[#a6a6a6]">
-            a digital space that showcases a person&#39;s best work, blog,
-            backgrounds, etc.
-          </p>
-          <div className="text-sm bg-[#18181b] border border-[#252529] rounded px-1 py-0.5 text-zinc-500 font-mono w-fit">
-            # NextJS
-          </div>
-        </Link>
-        <Link
-          href={"https://github.com/byanrkh/portfolio"}
-          className="border border-[#202024] p-4 rounded group hover:scale-[1.02] duration-75"
-        >
-          <h1 className="group-hover:underline">NextJS portfolio template</h1>
-          <p className="mt-2 mb-4 text-sm text-[#a6a6a6]">
-            portfolio template using Next.js to showcase personal or
-            professional projects, etc.
-          </p>
-          <div className="text-sm bg-[#18181b] border border-[#252529] rounded px-1 py-0.5 text-zinc-500 font-mono w-fit">
-            # NextJS
-          </div>
-        </Link>
+        {project.map((p) => {
+          return (
+            <Link
+              key={p.link}
+              href={p.link}
+              className="border border-[#202024] p-4 rounded group hover:scale-[1.02] duration-75"
+              target="_blank"
+            >
+              <h1 className="group-hover:underline">{p.label}</h1>
+              <p className="mt-2 mb-4 text-sm text-[#a6a6a6]">{p.summary}</p>
+              <ul className="flex gap-1">
+                {p.tags.map((tag) => {
+                  return (
+                    <li
+                      key={tag}
+                      className="text-sm bg-[#18181b] border border-[#252529] rounded px-1 py-0.5 text-zinc-500 font-mono w-fit"
+                    >
+                      # {tag}
+                    </li>
+                  );
+                })}
+              </ul>
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
